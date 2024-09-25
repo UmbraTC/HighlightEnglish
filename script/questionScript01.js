8e6a3de3c4dc278c7152d3a1368812a92497257f
UmbraTC <jrarcecms@gMAIL.COM>
2024-09-25 07:08:44 -0600

    quizz

--- script/questionScript01.js
+++ script/questionScript01.js
@@ -1,60 +1,57 @@
 let quiz01 = []
 
 let questionsW02_01 = [
-  '1. Have you ever been to South America?',
-  '2. Have you ever taken a long trip on a boat',
-  '3. Have you ever forgotten a friend’s name? ',
-  '4. Have you ever danced cumbias? ',
-  '5. What’s a sport you’ve always wanted to try? '
+  'Have you ever been to South America?',
+  'Have you ever taken a long trip on a boat',
+  'Have you ever forgotten a friend’s name? ',
+  'Have you ever danced cumbias? ',
+  'What’s a sport you’ve always wanted to try? '
 ]
 let questionsW02_02 = [
-  '1. When are you outgoing? When are you shy?',
-  '2. Do you prefer serious or easygoing people? Why?',
-  '3. When are you on time? Are you ever late? ',
-  '4. What are people in your family like? '
+  'When are you outgoing? When are you shy?',
+  'Do you prefer serious or easygoing people? Why?',
+  'When are you on time? Are you ever late? ',
+  'What are people in your family like? '
 ]
 
 let questionsW02_03 = [
-  '1. Have you ever taken a surfing class?',
-  '2. Have you ever sung in front of an audience?',
-  '3. Have you ever seen one of the Twilight movies?',
-  '4. Have you ever tried a food you didn’t like?',
-  '5. What’s a country you’ve never been to but would like to visit? '
+  'Have you ever taken a surfing class?',
+  'Have you ever sung in front of an audience?',
+  'Have you ever seen one of the Twilight movies?',
+  'Have you ever tried a food you didn’t like?',
+  'What’s a country you’ve never been to but would like to visit? '
 ]
 let questionsW02_04 = [
-  '1. When is it important to be patient?',
-  '2. When is it the most important to be polite?',
-  '3. When are you creative?',
-  '4. What are your friends like?',
-  '5. What’s a sport you’ve always wanted to try? '
+  'What are young people like around here?',
+  'Is it ever OK to be selfish? When? ',
+  'When are your friends competitive?',
+  'When are you a hard worker?'
 ]
 let questionsW02_05 = [
-  '1. Have you ever found some money?',
-  '2. Have you ever lost something valuable?',
-  '3. Have you ever been to Paris?',
-  '4. Have you ever been in a hospital?',
-  '5. What’s a subject you’ve always wanted to know more about? '
+  'Have you ever found some money?',
+  'Have you ever lost something valuable?',
+  'Have you ever been to Paris?',
+  'Have you ever been in a hospital?',
+  'What’s a subject you’ve always wanted to know more about? '
 ]
 let questionsW02_06 = [
-  '1. Have you ever been to South America?',
-  '2. Have you ever taken a long trip on a boat',
-  '3. Have you ever forgotten a friend’s name? ',
-  '4. Have you ever danced cumbias? ',
-  '5. What’s a sport you’ve always wanted to try? '
+  'What are people in your neighborhood like?',
+  'Do you learn languages easily? Or is it difficult?',
+  'Do you think most people are honest? Why orwhy not? ',
+  'How could you be more organized? '
 ]
 let questionsW02_07 = [
-  '1. Have you ever met a famous person?',
-  '2. Have you ever had Russian food?',
-  '3. Have you ever done something embarrassing? ',
-  '4. Have you ever won a contest?',
-  '5. What’s a skill or a talent you’ve always wanted to have?'
+  'Have you ever met a famous person?',
+  'Have you ever had Russian food?',
+  'Have you ever done something embarrassing? ',
+  'Have you ever won a contest?',
+  'What’s a skill or a talent you’ve always wanted to have?'
 ]
 let questionsW02_08 = [
-  '1. Have you ever been to South America?',
-  '2. Have you ever taken a long trip on a boat',
-  '3. Have you ever forgotten a friend’s name? ',
-  '4. Have you ever danced cumbias? ',
-  '5. What’s a sport you’ve always wanted to try? '
+  'When is it important to be patient?',
+  'When is it the most important to be polite?',
+  'When are you creative?  ',
+  'What are your friends like?'
 ]
 let questGen = (arrayQuest) =>{
   for (let i = 0; i< arrayQuest.length; i++){
@@ -80,8 +77,8 @@ let btt_Press  = function() {
    document.getElementById('q04').innerHTML  =  questGen(questionsW02_04)
    document.getElementById('q05').innerHTML  = questGen(questionsW02_05)
    document.getElementById('q06').innerHTML  = questGen(questionsW02_06)
-   document.getElementById.q07.innerHTML  = questGen(questionsW02_07)
-   document.getElementById.q08.innerHTML  = questGen(questionsW02_08)
+   document.getElementById('q07').innerHTML  = questGen(questionsW02_07)
+   document.getElementById('q08').innerHTML  = questGen(questionsW02_08)
 };
 
 button1.addEventListener('click', btt_Press);
