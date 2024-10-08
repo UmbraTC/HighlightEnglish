95d554cc5f651b5779ecca9dd8c4f29c43506668
UmbraTC <jrarcecms@gMAIL.COM>
2024-10-08 00:47:46 -0600

    df

--- Free codecamp to React/script/free_code_camp to React.js
+++ Free codecamp to React/script/free_code_camp to React.js
@@ -1,15 +1,10 @@
 // free_code_camp to react
-
 const page = (
     <div>
-        <h1> Umbra the wonderful cat</h1>
-        <ul>
-        <li>Pet</li>
-        <li>Magic cat</li>
-        <li>Pet owner owner</li>
-        </ul>
-  </div>
+     </div>
 )
+//
+
 
 ReactDOM.render(page, document.getElementById('root'))
 
